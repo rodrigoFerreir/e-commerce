279a823e0f757140e4e1847099621e214ba46854
rodrigoFerreir <rodrigoferreirasilva1997@hotmail.com>
2021-10-28 13:03:39 -0300

    enviando user

--- pystore/settings.py
+++ pystore/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # myapps
+    'users.apps.UsersConfig',
+
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# definindo user model padrão
+AUTH_USER_MODEL = "users.User"
